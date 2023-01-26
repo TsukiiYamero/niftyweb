53bfdffecca167d17b54a1b07e7cb70e0c9ce8b6
SaiKisho <diegosferas@gmail.com>
2023-01-26 16:06:41 -0500

    Refactor Navigation when page not found

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,16 +1,40 @@
-import { useRouteError } from 'react-router-dom';
+import { Box, Link } from '@mui/material';
+import { useNavigate, useRouteError } from 'react-router-dom';
 
 export const ErrorPage = () => {
     const error: any = useRouteError();
+    const navigateTo = useNavigate();
     console.error(error);
 
+    const onClickRedirect = () => {
+        navigateTo('/');
+    };
+
     return (
-        <div>
+        <Box
+
+            sx={{
+                padding: '29px',
+
+            }}
+        >
             <h1>Oops!</h1>
             <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>Page {error?.statusText || error?.message}</i>
-            </p>
-        </div>
+            <i>Page {error?.statusText || error?.message}</i>
+            <Box
+                sx={{
+                    typography: 'body1',
+                    '& > :not(style) + :not(style)': {
+                        ml: 2,
+                    },
+                    paddingBottom: '20px'
+                }}
+                onClick={onClickRedirect}
+            >
+                <Link href="#">Return Home</Link>
+            </Box>
+
+            <i>Design in construction</i>
+        </Box>
     );
 };
